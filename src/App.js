5964f5415a5fc83368d40643939f1a2860b54a92
Kapish <kapish.k.1@gmail.com>
2017-04-25 20:56:41 -0400

    added search bar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,39 @@ import Logo from './Logo.jsx';
 import Navigation from './Navigation.jsx';
 
 class App extends Component {
+
+  apiKEY: '86381d33f1a2b61021d5b6fe42800d6e'
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      searchURL: ""
+    };
+  }
+
+  handleKeyUp = (e) => {
+    if (e.key === 'Enter' && this.state.searchTerm !== "") {
+      var searchURL = "search/multi?query=" + this.state.searchTerm + "&api_key" + this.apiKey;
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({searchTerm: e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-          <Logo />
-          <Navigation />
+        <Logo />
+        <Navigation />
+        <div id="search" className="Search">
+          <input
+            onKeyUp={this.handleKeyUp}
+            onChange={this.handleChange}
+            type="search"
+            placeholder="Search"
+            value={this.state.searchTerm} />
+        </div>
       </div>
     );
   }
