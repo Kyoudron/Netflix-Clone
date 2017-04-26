4cca7671dee9c25f5eba005841d6cb56cbaf7da9
Kapish <kapish.k.1@gmail.com>
2017-04-26 19:35:45 -0400

    test

--- src/App.js
+++ src/App.js
@@ -13,20 +13,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      movies: [
-        {
-          id: 1,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 2,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 3,
-          url: 'http://fakeimg.pl/300/'
-        }
-      ]
+      movies: []
     }
   }
 
