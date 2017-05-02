9a2ca3515685fad46d22833fe05a1ce66e0b4509
Kapish <kapish.k.1@gmail.com>
2017-05-01 21:16:16 -0400

    fixing movie render

--- src/ListToggle.jsx
+++ src/ListToggle.jsx
@@ -10,7 +10,24 @@ class ListToggle extends Component {
     }
   }
 
-  render() {
+  handleClick = () => {
+    if(this.state.toggled === true) {
+      this.setState({toggled: false});
+    } else {
+      this.setState({toggled: true});
+    }
+  }
 
+  render() {
+    return (
+      <div onClick={this.handleClick} data-toggled={this.state.toggled} className="ListToggle">
+        <div>
+          <i className="fa fa-fw fa-plus"></i>
+          <i className="fa fa-fw fa-check"></i>
+        </div>
+      </div>
+    );
   }
 }
+
+export default ListToggle;
